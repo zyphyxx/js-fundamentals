d11e916e3475cfbb34a79fdf7816049b39beb33f
Edgar Jesus <mendesbr92@gmail.com>
2023-12-15 18:01:23 -0300

    feat: objeto Math

---
+++ src/1-beginner/index/math.js
@@ -0,0 +1,10 @@
+// OBJETO MATH
+
+let num1 = 9.54578;
+let num2 = Math.floor(num1); // arredondar para baixo
+let num3 = Math.ceil(num1); // arredondar para cima
+let num4 = Math.round(num1); // arredondar de forma automatica
+
+const aleatorio = Math.random() * (10 - 5);
+
+console.log(Math.round(aleatorio));
