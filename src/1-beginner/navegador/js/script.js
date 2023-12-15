28345bc0037a9bea744869e8744e15d09dab354b
Edgar Jesus <mendesbr92@gmail.com>
2023-12-14 22:53:37 -0300

    feat: aprendendo a utilizar o dom

---
+++ src/1-beginner/navegador/js/script.js
@@ -0,0 +1,11 @@
+
+
+let numOne = window.prompt('Digite um numero: ')
+let numTwo = window.prompt('Digite o segundo numero:')
+
+let result = parseInt(numOne) + parseInt(numTwo);
+console.log(result);
+
+window.alert(`O resultado foi: ${result}`);
+
+window.confirm('Deseja realmente apagar?')
