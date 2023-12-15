dc7e392947d446efa4c6bf3718017fbd86c477ac
Edgar Jesus <mendesbr92@gmail.com>
2023-12-14 21:14:15 -0300

    feat: aulas sobre const e let

---
+++ src/1-beginner/const.js
@@ -0,0 +1,10 @@
+// CONSTANTES COM CONST
+
+const nome = 'Edgar';
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+
+console.log(resultado);
+
---
+++ src/1-beginner/let.js
@@ -0,0 +1,8 @@
+// VARIAVEIS COM LET
+
+let  nome = 'Edgar Mendes';
+let  declarar; // Declarar a variavel
+
+
+console.log(nome);
+console.log(declarar); // retorna undefined
