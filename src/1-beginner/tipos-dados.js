dd5fbbe693ca89b9a3c75729ac66004789689a28
Edgar Jesus <mendesbr92@gmail.com>
2023-12-14 21:55:15 -0300

    feat: aula sobre tipos de dados primitivos

---
+++ src/1-beginner/tipos-dados.js
@@ -0,0 +1,15 @@
+// TIPOS DE DADOS PRIMITIVOS
+
+const nome = 'Edgar'; // string
+
+const numero = 10; // number
+
+let nomeAluno; // undefined -> não aponta para local nunhum na memoria
+
+const aprovado = false; // Bollean true ou false (logico)
+
+console.log(typeof nome)
+console.log(typeof numero);
+console.log(typeof  nomeAluno);
+console.log(typeof aprovado)
+
