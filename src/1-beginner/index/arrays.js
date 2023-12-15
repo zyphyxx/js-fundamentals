4ab264530b3b6289df75ba54b1f8b29b197cfc9f
Edgar Jesus <mendesbr92@gmail.com>
2023-12-15 19:59:51 -0300

    feat: arrays basico

---
+++ src/1-beginner/index/arrays.js
@@ -0,0 +1,17 @@
+// ARRAYS BASICO
+
+let alunos = ['Edgar','Anny','Leo'];
+
+console.log(`1 - ${alunos[0]}`);
+console.log(`2 - ${alunos[1]}`);
+console.log(`3 - ${alunos[2]}`);
+
+alunos.push('Gunts'); // Adicionar um elemento ao final do array
+alunos.unshift('Luiza') // Adiciona no começo do array
+
+alunos.pop(); // Apaga um elemento no final
+alunos.shift(); // Apaga um elemento no começo
+
+console.log(`tamanho da array:${alunos.length}`); // tamanho da array
+console.log(`alunos na array: ${alunos}`)
+
