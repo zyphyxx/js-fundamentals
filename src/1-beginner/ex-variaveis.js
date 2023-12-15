fc10a60c3ab1b7b0ed292c24dea2c12db1d0fe10
Edgar Jesus <mendesbr92@gmail.com>
2023-12-15 17:40:36 -0300

    chore(index): aula sobre const e let

---
+++ src/1-beginner/ex-variaveis.js
@@ -0,0 +1,15 @@
+    // EXERCICIO COM VARIAVEIS
+
+
+    let varA = 'A'; // B
+    let varB = 'B'; // C
+    let varC = 'C'; // A
+
+    console.log(varA,varB,varC);
+
+    const a = varA;
+    const b1 = (varA = varB);
+    const c1 = (varB = varC);
+
+
+    console.log(b1,c1,a);
