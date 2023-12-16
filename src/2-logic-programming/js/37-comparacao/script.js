66a3f12b3b182fe32fccfdb66391e1364df7f9cd
Edgar Jesus <mendesbr92@gmail.com>
2023-12-16 14:53:58 -0300

    feat: operadores de comparacao

---
+++ src/2-logic-programming/js/37-comparacao/script.js
@@ -0,0 +1,12 @@
+/*
+    OPERADORES DE COMPARAÇÃO:
+
+    >   maior que
+    >=  maior que ou igual a
+    <   menor que
+    <=  menor que ou igual a
+    === igualdade estrita (valor e tipo)
+    !== diferente estrita (valor e tipo)
+
+ */
+
