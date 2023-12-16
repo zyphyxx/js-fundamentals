321f189850590d3ab4046b208f4205760bc536c4
Edgar Jesus <mendesbr92@gmail.com>
2023-12-16 15:09:25 -0300

    feat: operadores logicos

---
+++ src/2-logic-programming/js/38-op-logicos/script.js
@@ -0,0 +1,16 @@
+/*
+    OPERADORES LÓGICOS:
+
+    && -> AND -> E   -- toda logica tem que ser verdadeira
+    || -> OR  -> OU  -- uma das expressões pode ser verdadeira
+    !  -> NOT -> NÃO
+ */
+
+const and = true && true;
+console.log(and);
+
+const or = false || true;
+console.log(or);
+
+const not = !true;
+console.log(not);
