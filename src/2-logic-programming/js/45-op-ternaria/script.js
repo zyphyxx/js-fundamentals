b2aa4d54ec454f5552e49c73fb80a2cef9ecb013
Edgar Jesus <mendesbr92@gmail.com>
2023-12-18 19:06:10 -0300

    feat: operacoes ternarias

---
+++ src/2-logic-programming/js/45-op-ternaria/script.js
@@ -0,0 +1,6 @@
+    // OPERAÇOES TERNARIAS
+// (condiçao) ? 'valor para verdadeiro' : 'valor para falso'
+const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuario normal';
+
+console.log(nivelUsuario);
