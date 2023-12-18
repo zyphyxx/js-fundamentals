ca83a12fbf6b426f6cf6257282ba510393f1f99f
Edgar Jesus <mendesbr92@gmail.com>
2023-12-18 20:50:02 -0300

    feat: switch and case

---
+++ src/2-logic-programming/js/47-switch-case/script.js
@@ -0,0 +1,33 @@
+// SWITCH / CASE
+
+const data = new Date();
+const diaSemana = data.getDay();
+let diaSemanaTexto;
+
+switch (diaSemana) {
+    case 0:
+        diaSemanaTexto = 'Domingo';
+        break;
+    case 1:
+        diaSemanaTexto = 'Segunda';
+        break;
+    case 2:
+        diaSemanaTexto = 'Terca';
+        break;
+    case 3:
+        diaSemanaTexto = 'Quarta';
+        break;
+    case 4:
+        diaSemanaTexto = 'Quinta';
+        break;
+    case 5:
+        diaSemanaTexto = 'Sexta';
+        break;
+    case 6:
+        diaSemanaTexto = 'Sabado';
+        break;
+    default:
+        diaSemanaTexto = ' ';
+}
+
+console.log(diaSemanaTexto);
