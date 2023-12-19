0ab1cf2454cd542d8b666ac8ad73be9ffdb22a28
Edgar Jesus <mendesbr92@gmail.com>
2023-12-19 20:00:27 -0300

    feat: for classico

---
+++ src/2-logic-programming/js/52-for/script.js
@@ -0,0 +1,7 @@
+// for classico - estrutura de repetição
+
+const frutas = ['maça', 'pera', 'uva', 'banana', 'melancia'];
+
+for (let i = 0; i <= 4; i++) {
+    console.log(`linha: ${frutas[i]}`);
+}
