39d0a937bfbffbcc2737a3fa36843629912e2871
Edgar Jesus <mendesbr92@gmail.com>
2023-12-20 19:36:06 -0300

    feat: exercicio com o for of

---
+++ src/2-logic-programming/js/56-for-out/script.js
@@ -0,0 +1,10 @@
+    // FOR OF - ESTRUTURA DE REPETIÇÃO
+
+const nome = ['Edgar','Sousa','Mendes'];
+    for (let valor of nome){
+        console.log(valor);
+    }
+
+    // for classico - geralmente com iteraveis (array ou strings)
+    // for in - retorna o indice ou chave (string, array, objeto)
+    // for of - retorna o valor em si (iteraveis, arrays ou strings)
