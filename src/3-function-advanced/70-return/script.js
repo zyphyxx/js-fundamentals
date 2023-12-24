ca26e1cfe646f343b1625314d772adbcefdfd0a8
Edgar Jesus <mendesbr92@gmail.com>
2023-12-23 21:32:33 -0300

    feat: retorno de funcoes

---
+++ src/3-function-advanced/70-return/script.js
@@ -0,0 +1,9 @@
+// RETORNO DAS FUNÇÕES
+
+console.log(criarMultiplicador(6, 5));
+
+function criarMultiplicador(num, mult) {
+    const multiplicador = mult;
+    const numero = num;
+    return num * mult;
+}
