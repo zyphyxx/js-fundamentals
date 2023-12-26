b8c3626b533be157797b44bf085319fa0c6605a4
Edgar Jesus <mendesbr92@gmail.com>
2023-12-26 20:30:32 -0300

    feat: criando uma calculadora

---
+++ src/3-function-advanced/exercices/76-model-calculadora/assets/js/main.js
@@ -0,0 +1,31 @@
+function criarCalculadora() {
+
+    return {
+        display: document.querySelector('.display'),
+
+
+        inicia() {
+            this.cliqueBotoes();
+        },
+
+        cliqueBotoes() {
+            document.addEventListener('click', function (e) {
+                const el = e.target;
+
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+            }.bind(this))
+        },
+
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        }
+
+
+    }
+
+}
+
+const calculadora = criarCalculadora();
+calculadora.inicia();
